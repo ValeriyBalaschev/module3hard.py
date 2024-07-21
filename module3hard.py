fa6736b13a8680095a475d4f65294fdf9de65f1d
ValeriyBalaschev <bwengs.ru@mail.ru>
2024-07-21 23:50:11 +0700

    Add files via upload

---
+++ module3hard.py
@@ -0,0 +1,34 @@
+# module3hard.py Дополнительное практическое задание по модулю: "Подробнее о функциях."
+
+
+def calculate_strukture_sum(a):
+    sum_ = 0
+    for i in a:
+        if isinstance(i, str):
+            sum_ += len(i)
+        if isinstance(i, int or float):
+            sum_ += i
+        if isinstance(i,list):
+            sum_ += calculate_strukture_sum(i)
+        if isinstance(i, tuple):
+            sum_ += calculate_strukture_sum(i)
+        if isinstance(i,dict ):
+            sum_ += calculate_strukture_sum(i.items())
+        if isinstance(i, set):
+            sum_ += calculate_strukture_sum(i)
+    return sum_
+
+
+
+
+data_structure = [
+    2,
+    6,
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+result = calculate_strukture_sum(data_structure)
+print(result)
